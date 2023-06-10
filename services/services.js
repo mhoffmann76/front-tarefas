1564fa5bed3d4f81b2b618cccf563c8364712988
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-06-10 20:20:54 -0300

    configurado views e services.js

--- services/services.js
+++ services/services.js
@@ -1,37 +1,53 @@
 const axios = require("axios");
 const { response } = require("express");
 
-
 module.exports = class Services {
   static async TarefaCreate(req, res) {
     let valores = req.body;
     const options = {
-      url: "https://replit.com/@mhoffmann761/listarTarefas/Cadastrar",
+      url: "https://listartarefas.mhoffmann761.repl.co/tarefas/Cadastrar",
       method: "POST",
       data: valores,
     };
 
     axios(options);
     const mensagem = "Cadastro realizado com sucesso!";
-    res.render("mensagem", {mensagem});
-    
-  };
-
+    res.render("mensagem", { mensagem });
+  }
 
   static async TarefaListar(req, res) {
     const options = {
-      url: "https://replit.com/@mhoffmann761/listarTarefas/tarefas",
+      url: "https://listartarefas.mhoffmann761.repl.co/tarefas/listar",
       method: "GET",
-      data: {}
-
+      data: {},
     };
-    axios(options).then(response=>{
+    axios(options).then((response) => {
       console.log(response.data);
       const tarefa = response.data;
-      res.render("tarefas/listar",{tarefa});
+      res.render("tarefas/listar", { tarefa });
     });
-  };
+  }
+
+  static async TarefaUpdate(req, res) {
+    let valores = req.body;
+    const options = {
+      url: `https://listartarefas.mhoffmann761.repl.co/tarefas/${valores.id_tarefa}`,
+      method: "PUT",
+      data: valores,
+    };
+    axios(options);
+    const mensagem = "Registro atualizado com sucesso";
+    res.render("mensagem", { mensagem });
+  }
+
+  static async TarefaDelete(req, res) {
+    let id_tarefa = req.body.id_tarefa;
+    const options = {
+      url: `https://listartarefas.mhoffmann761.repl.co/tarefas/${id_tarefa}`,
+      method: "DELETE",
+    };
+    axios(options);
+    const mensagem = "Tarefa excluída com sucesso!";
+    res.render("mensagem", { mensagem });
+  }
 };
-
-
-
